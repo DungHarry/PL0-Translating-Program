8010398afc8d4cd639ccf75f7f866d9918d4dbe6
Dung Harry <lyvietdung92@gmail.com>
2013-10-05 14:20:44 +0700

    PL0 translating source code - 5th, October 2013

---
+++ SemanticAns.c
@@ -0,0 +1,14 @@
+/*
+	Author: Lý Việt Dũng - Hanoi University of Science and Technology
+	Date created: 3rd, October 2013
+	Operating System: Ubuntu 13.04 Desktop x86_64
+	Compiler: GNU compiler 4.7.3
+	Program: PL0 translate program
+	Module: Semantic analysis
+
+	Description: this is source code file for semantic analysis process
+*/
+
+#include "SemanticAns.h"
+
+
---
+++ SemanticAns.h
@@ -0,0 +1,27 @@
+/*
+	Author: Lý Việt Dũng - Hanoi University of Science and Technology
+	Date created: 3rd, October 2013
+	Operating System: Ubuntu 13.04 Desktop x86_64
+	Compiler: GNU compiler 4.7.3
+	Program: PL0 translate program
+	Module: Semantic analysis
+
+	Description: this is header file for semantic analysis process
+*/
+
+#ifndef _SEMANTIC_ANS_H_
+#define _SEMANTIC_ANS_H_
+
+#include "SyntaxAns.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdio.h>
+//Extern some variable here
+
+extern token_str tokenTable[MAX_TOKEN];
+extern iLineSemanticError = 1;
+extern iColumnSemanticError = 1;
+
+//Report for some structs hold information about variable, parameter and producedure here
+
+#endif
